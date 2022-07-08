c295910d2442ae0e3cf523a4fc1bad2bd8674fc1
Sholpan Baltabayeva <murat@MacBook-Sholpan.local>
2022-07-08 23:00:03 +0600

    Updated

--- vlog_app/models.py
+++ vlog_app/models.py
@@ -118,12 +118,12 @@ class SiteInfo(models.Model):
         return super().save(*args, **kwargs)
 
 
-SITE_INFO = {
-    'contacts': Contact.objects.all(),
-    'site_info': SiteInfo.objects.all().first()
-}
-
 # SITE_INFO = {
-#     'contacts': None,
-#     'site_info': None
+#     'contacts': Contact.objects.all(),
+#     'site_info': SiteInfo.objects.all().first()
 # }
+
+SITE_INFO = {
+    'contacts': None,
+    'site_info': None
+}
